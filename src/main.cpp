6085d52289d5c6a9241cd1273fa805c439a2efe5
Goran Kecman <lestrigonac@hotmail.com>
2018-02-28 14:19:38 +0100

    Added serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,40 @@
 #include "mbed.h"
 #include "test.h"
 
-DigitalOut myled(LED1);
 
-int value = 1;
+#define LED_GREEN   p21
+#define LED_RED     p22
+#define LED_BLUE    p23
+#define BUTTON_PIN  p17
+
+//#define UART_TX     p9
+#define UART_TX     p3
+//#define UART_RX     p11
+#define UART_RX     p4
+ 
+DigitalOut blue(LED_BLUE);
+DigitalOut green(LED_GREEN);
+DigitalOut red(LED_RED);
+InterruptIn button(BUTTON_PIN);
+Serial pc(UART_TX, UART_RX);
+
+void detect(void)
+{
+    pc.printf("Button pressed\n");  
+    green = !green;
+}
 
 int main() {
+    green = 1;
+    red = 1;
+    button.fall(detect);
+    pc.baud(9600);
+    pc.printf("Start...\n");
+
     while(1) {
-        myled = value;
-        value = test_toggle_value(value);
+        blue = 1;
         wait(0.2);
-        myled = value;
-        value = test_toggle_value(value);
+        blue = 0;
         wait(0.2);
     }
 }
