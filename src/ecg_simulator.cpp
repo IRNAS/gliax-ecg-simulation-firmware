d17275f0f88cd7491e2c095e14e3069d8bc5245e
Goran Kecman <lestrigonac@hotmail.com>
2018-03-02 15:43:06 +0100

    Changes tab's to spaces, copy error removed

--- src/ecg_simulator.cpp
+++ src/ecg_simulator.cpp
@@ -37,74 +37,74 @@ void ecg_sender_init(void)
 
 void ecg_sender_send(Serial *pc)
 {
-	//Create header and calculate data size
-	uint8_t header[Packetizer::HEADER_SIZE + sizeof(ECGHeader)];
-	ECGHeader *ecgHeader = (ECGHeader *)(header + Packetizer::HEADER_SIZE);
+    //Create header and calculate data size
+    uint8_t header[Packetizer::HEADER_SIZE + sizeof(ECGHeader)];
+    ECGHeader *ecgHeader = (ECGHeader *)(header + Packetizer::HEADER_SIZE);
 
-	ecgHeader->lsbInMv = ecgSenderData.currLsbInMv * (1 << ECG_CHOP_BITS);
-	ecgHeader->samplingFrequency = ecgSenderData.currFrequency;
-	ecgHeader->channelCount = ecgSenderData.selectedChannels;
+    ecgHeader->lsbInMv = ecgSenderData.currLsbInMv * (1 << ECG_CHOP_BITS);
+    ecgHeader->samplingFrequency = ecgSenderData.currFrequency;
+    ecgHeader->channelCount = ecgSenderData.selectedChannels;
 
-	uint32_t blockSize = (ecgHeader->channelCount + 1) * 3;
+    uint32_t blockSize = (ecgHeader->channelCount + 1) * 3;
     //Number 15 is selected as an example of sending data with this simple simulator
     //When selecting this number, we have taken into accound value of blockSize and to begin with channelCount
     //Actual size value depends on data in circular buffer at this moment. 
     //This number might be changed when testing with app
     //Original implementation is available in ADS1298.cpp file on line 254
-	uint32_t size = 15;
+    uint32_t size = 15;
     size = size - size % blockSize;
 
-	if (size > ECG_MAX_SEND_SIZE)
+    if (size > ECG_MAX_SEND_SIZE)
     {
-		size = ECG_MAX_SEND_SIZE;
+        size = ECG_MAX_SEND_SIZE;
     }
 
-	ecgHeader->sampleCount = size / blockSize;
+    ecgHeader->sampleCount = size / blockSize;
 
-	if (size == 0)
+    if (size == 0)
     {
-		return;
-	}
+        return;
+    }
 
-	//Initialize the compressor
-	compressFifo.reset();
-	ecgPredictor.reset();
-	ecgPredictor.setNumChannels(ecgHeader->channelCount);
-	compressor.setNumChannels(ecgHeader->channelCount);
+    //Initialize the compressor
+    compressFifo.reset();
+    ecgPredictor.reset();
+    ecgPredictor.setNumChannels(ecgHeader->channelCount);
+    compressor.setNumChannels(ecgHeader->channelCount);
 
-//	ADS1298 &ecg=ADS1298::instance();
+//  ADS1298 &ecg=ADS1298::instance();
 
-	//Compress
-	for (unsigned pos = 0; pos < size; pos += blockSize)
+    //Compress
+    for (unsigned pos = 0; pos < size; pos += blockSize)
     {
-		//ecg.getSample(sampleOfChannels);
+        //ecg.getSample(sampleOfChannels);
 
-		if (ecgSenderData.testSignal)
+        if (ecgSenderData.testSignal)
         {
-			for (int a = 0; a < ecgHeader->channelCount; a++)
+            for (int a = 0; a < ecgHeader->channelCount; a++)
             {
-				ecgSenderData.sampleOfChannels[a] = testGenerator.getSample(testGenerator.getPeriod()*a/ecgHeader->channelCount);
-				testGenerator.next();
-			}
-		}
+                ecgSenderData.sampleOfChannels[a] = testGenerator.getSample(testGenerator.getPeriod()*a/ecgHeader->channelCount);
+                testGenerator.next();
+            }
+        }
 
-		for (int a = 0; a < ecgHeader->channelCount; a++){
-			ecgSenderData.sampleOfChannels[a] >>= ECG_CHOP_BITS;
-		}
-		//Compress
-		compressor.putSample(ecgSenderData.sampleOfChannels);
-	}
+        for (int a = 0; a < ecgHeader->channelCount; a++){
+            ecgSenderData.sampleOfChannels[a] >>= ECG_CHOP_BITS;
+        }
+        //Compress
+        compressor.putSample(ecgSenderData.sampleOfChannels);
+    }
 
-	ecgHeader->numBits = compressFifo.getAvailableBits();
-	size = (ecgHeader->numBits + 7) / 8;
+    ecgHeader->numBits = compressFifo.getAvailableBits();
+    size = (ecgHeader->numBits + 7) / 8;
 
 #ifdef DEBUG
     pc->printf("Sending packet.\n");
 #endif
 
-	//Send header
-	packetizer.startPacket(header, Packetizer::ECG, (uint16_t)(size+sizeof(ECGHeader)));
-	packetizer.checksumBlock((uint8_t*)ecgHeader, sizeof(ECGHeader));
+    //Send header
+    packetizer.startPacket(header, Packetizer::ECG, (uint16_t)(size+sizeof(ECGHeader)));
+    packetizer.checksumBlock((uint8_t*)ecgHeader, sizeof(ECGHeader));
 #ifdef DEBUG
     pc->printf("Packet size: %d \n", (Packetizer::HEADER_SIZE + sizeof(ECGHeader)));
 #endif
@@ -114,7 +114,7 @@ void ecg_sender_send(Serial *pc)
     }
     
     //Calculate checksum over compressBuffer and add to final checksum value
-	packetizer.checksumBlock(ecgSenderData.compressBuffer, size);
+    packetizer.checksumBlock(ecgSenderData.compressBuffer, size);
     //Send compressBuffer (data)
 #ifdef DEBUG
     pc->printf("\nCompressBuffer Size: %d\n", size);
@@ -124,8 +124,8 @@ void ecg_sender_send(Serial *pc)
         pc->putc(ecgSenderData.compressBuffer[i]);
     }
 
-	//Send checksum
-	Packetizer::Checksum chksum = packetizer.getChecksum();
+    //Send checksum
+    Packetizer::Checksum chksum = packetizer.getChecksum();
 #ifdef DEBUG
     pc->printf("Checksum Size: %d\n", sizeof(chksum));
 #endif
