31905b28295fcbdf264395270ea1eec550e46d00
Goran Kecman <lestrigonac@hotmail.com>
2018-03-05 13:56:06 +0100

    Updated UART definitions to support generic mbed board UART connected to PC

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,23 @@
 
 //#define DEBUG
 
-#define LED_GREEN   p21
-#define LED_RED     p22
-#define LED_BLUE    p23
-#define BUTTON_PIN  p17
+DigitalOut led(LED1);
+Serial pc(USBTX, USBRX);
 
-//#define UART_TX     p9
-#define UART_TX     p3
-//#define UART_RX     p11
-#define UART_RX     p4
-
-DigitalOut blue(LED_BLUE);
-DigitalOut green(LED_GREEN);
-DigitalOut red(LED_RED);
-InterruptIn button(BUTTON_PIN);
-
-Serial pc(UART_TX, UART_RX);
-
-void detect(void)
+int main() 
 {
-    pc.printf("Button pressed\n");  
-    green = !green;
-}
-
-int main() {
-    green = 1;
-    red = 1;
-    blue = 0;
-    button.fall(detect);
-
+    //Initialize ecg
     ecg_sender_init();
-
+    //Initialize serial communication
     pc.baud(115200);
 #ifdef DEBUG
     pc.printf("Start...\n");
 #endif
 
-    while(1) {
+    while(1) 
+    {
+        //Send ecg data periodically
         ecg_sender_send(&pc); 
         wait(0.002048);
-        //blue = !blue;
     }
 }
